36c15e679d6e865dc361b4bdb26ed7dd4c3397c3
InanisV <37214590+InanisV@users.noreply.github.com>
2018-12-19 14:31:34 +0800

    Update Timer.v

--- timer/Timer.v
+++ timer/Timer.v
@@ -18,10 +18,12 @@ wire tmp1,tmp2,tmp3,tmp4;
 wire clkout;
 clock_div c1(clk,rst_n,clkout);
 
-counter_1 s2(clkout, in, rst_n, tmp1, out_s2);
-counter_2 s1(clkout, tmp1, rst_n, tmp2, out_s1);
-counter_1 m2(clkout, tmp2, rst_n, tmp3, out_m2);
-counter_2 m1(clkout, tmp3, rst_n, tmp4, out_m1);
+//counter_1 s2(clkout, in, rst_n, tmp1, out_s2);
+//counter_2 s1(clkout, tmp1, rst_n, tmp2, out_s1);
+//counter_1 m2(clkout, tmp2, rst_n, tmp3, out_m2);
+//counter_2 m1(clkout, tmp3, rst_n, tmp4, out_m1);
+counter_12 s(clkout, in, rst_n, tmp1, out_s1, out_s2);
+counter_12 m(clkout, tmp1, rst_n, tmp4, out_m1, out_m2);
 counter_3 h0(clkout, tmp4, rst_n, out_h1, out_h2);
 
 scan_seg seg1(rst_n,clk,out_s2,out_s1,out_m2,out_m1,out_h2,out_h1,seg_en,seg_out);
