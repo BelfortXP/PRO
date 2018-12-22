0fdb9c6d56c44cf6a42ccfe660b82896ace4da6b
InanisV <37214590+InanisV@users.noreply.github.com>
2018-12-22 21:35:47 +0800

    Update clock_set.v

--- timer/clock_set.v
+++ timer/clock_set.v
@@ -22,21 +22,13 @@
 
 module clock_set(
     input set_min,
-    input set_hour,
     input [3:0] clock1,
     input [3:0] clock2,
     output reg [3:0] clock_min1,
-    output reg [3:0] clock_min2,
-    output reg [3:0] clock_hour1,
-    output reg [3:0] clock_hour2
+    output reg [3:0] clock_min2
     );
-    always @(set_min or set_hour)
-        if(set_hour)
-        begin
-            clock_hour1 <= clock1;
-            clock_hour2 <= clock2;
-        end    
-        else if(set_min)
+    always @(set_min)
+        if(set_min)
         begin
             clock_min1 <= clock1;
             clock_min2 <= clock2;
